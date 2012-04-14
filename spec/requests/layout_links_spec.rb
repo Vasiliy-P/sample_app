dbe6831746439503cfd8f6a5830f47d20ed75327
Vasiliy P. Pupkin <vasiliy-pupkin.pupkin@yandex.ru>
2012-04-14 22:26:07 +0400

    Finished first cut of the user model

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -12,12 +12,12 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Contact")
   end
 
-  it "should have a Home page at '/about'" do
+  it "should have a About page at '/about'" do
     get '/about'
     response.should have_selector('title', :content => "About")
   end
 
-   it "should have a Home page at '/help'" do
+   it "should have a Help page at '/help'" do
     get '/help'
     response.should have_selector('title', :content => "Help")
   end
@@ -27,6 +27,19 @@ describe "LayoutLinks" do
     response.should have_selector('title', :content => "Sign up")
   end
 
+    it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    response.should have_selector('title', :content => "About")
+    click_link "Help"
+    response.should have_selector('title', :content => "Help")
+    click_link "Contact"
+    response.should have_selector('title', :content => "Contact")
+    click_link "Home"
+    response.should have_selector('title', :content => "Home")
+    click_link "Sign up now!"
+    response.should have_selector('title', :content => "Sign up")
+ end
 end
 #  describe "GET /layout_links" do
 #    it "works! (now write some real specs)" #do
