9ed2c3192a5ec6f6cff4a7014446b5242dae5f8b
Vasiliy P. Pupkin <vasiliy-pupkin.pupkin@yandex.ru>
2012-04-18 23:51:12 +0400

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem 'gravatar_image_tag'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
@@ -44,4 +45,5 @@ end
 group :test do
    gem 'rspec-rails'
    gem 'webrat'
+   gem 'factory_girl_rails'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,15 @@
+#Используя ':user', указываем Factory Girl на необходимость симулировать
+#Factory.define :user do |user|
+#  user.name "Ruslan"
+#  user.email "ruslan@example.com"
+#  user.password "qwerty"
+#  user.password_confirmation "qwerty"
+#end
+FactoryGirl.define do
+     factory :user do
+     name 'Ruslan'
+     email 'ruslan@example.com'
+     password 'qwerty'
+     password_confirmation 'qwerty'
+     end
+end
